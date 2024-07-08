f909abf20f2ebcc4117ba3af4caf1b6e380f4457
kevin-s-le <kevin.s.le@seattlecolleges.edu>
2024-07-07 22:16:33 -0700

    first push

---
+++ script.js
@@ -0,0 +1,15 @@
+// BMI Calculator
+// 1 inch = 2.54 centimeters
+// 1 pound = 0.4536 kilograms
+
+function BMI() {
+    let height = parseFloat(document.getElementById("height").value);
+    let weight = parseFloat(document.getElementById("weight").value);
+    let heightInCentimeters = height * 2.54;
+    let weightInKilograms = weight * 0.4536;
+
+    // Calculating for BMI
+    let heightInMeters = heightInCentimeters / 100;
+    let result = weightInKilograms / (heightInMeters * heightInMeters);
+    document.getElementById("result").innerHTML = 'BMI Result: ' + result;
+}
